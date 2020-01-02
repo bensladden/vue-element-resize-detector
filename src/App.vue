3ed3555f8695057a4dd6fddf492d78265a53f772
bensladden <ben.sladden@dematec.com.au>
2020-01-02 20:48:26 +1030

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Vue Element Resize Detector</h1>
+    <div v-resize @resize="onResize">This divs with is: {{ width }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      width: 0
+    };
+  },
+  methods: {
+    onResize(e) {
+      console.log("resize event", e.detail.width);
+      this.width = e.detail.width;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+import resize from "./directive/elementResizeDetector";
+
+Vue.config.productionTip = false;
+
+Vue.directive("resize", resize);
+
+new Vue({
+	render: h => h(App)
+}).$mount("#app");
