4f76eb302086579e02c656a993e81ed7fe4e3d1d
bensladden <ben.sladden@dematec.com.au>
2020-01-11 16:59:54 +1030

    chore:update deps

--- dist/vue-element-resize-detector.common.js
+++ dist/vue-element-resize-detector.common.js
@@ -213,7 +213,12 @@ module.exports = function(options) {
                         //So if it is not present, poll it with an timeout until it is present.
                         //TODO: Could maybe be handled better with object.onreadystatechange or similar.
                         if(!element.contentDocument) {
-                            setTimeout(function checkForObjectDocument() {
+                            var state = getState(element);
+                            if (state.checkForObjectDocumentTimeoutId) {
+                                window.clearTimeout(state.checkForObjectDocumentTimeoutId);
+                            }
+                            state.checkForObjectDocumentTimeoutId = setTimeout(function checkForObjectDocument() {
+                                state.checkForObjectDocumentTimeoutId = 0;
                                 getDocument(element, callback);
                             }, 100);
 
@@ -297,6 +302,11 @@ module.exports = function(options) {
         } else {
             element.removeChild(getObject(element));
         }
+
+        if (getState(element).checkForObjectDocumentTimeoutId) {
+            window.clearTimeout(getState(element).checkForObjectDocumentTimeoutId);
+        }
+
         delete getState(element).object;
     }
 
